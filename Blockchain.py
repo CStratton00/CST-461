61dfac1601bd7a97334ef79efee4b8b41fa7e2ab
Collin Stratton <Collin.Stratton00@gmail.com>
2022-01-12 06:02:36 -0800

    added comments and started signatures and verification

--- Blockchain.py
+++ Blockchain.py
@@ -1,33 +1,51 @@
+"""
+Collin Stratton
+Isac Artzi
+CST-461
+
+Create a basic blockchain
+"""
+
+# imports
 import datetime
 
+# block class
 class Block:
-    def __init__(self, index, data, date, data_hash, previous_hash):
+    # constructor containing everything stored in the block
+    def __init__(self, index, data, timestamp, previous_hash, validator, signature):
         self.id = index
         self.data = data
-        self.date = date
-        self.hash = data_hash
+        self.timestamp = timestamp
+        self.hash = hash((index, data, timestamp, previous_hash))
         self.previous_hash = previous_hash
+        self.validator = validator
+        self.signature = signature
 
+# blockchain class
 class Blockchain:
+    # constructor creating the chain and initial block
     def __init__(self):
         self.chain = []
         self.add_block()
         pass
 
+    # add block to the chain
     def add_block(self, data=None):
         # check if there is a previous block
-        previous_hash = 0000000000000000000
+        previous_hash = 0
         if len(self.chain) > 0:
             previous_hash = self.chain[-1].hash
 
-        block = Block(len(self.chain), data, datetime.datetime.now(), hash(data), previous_hash)
+        # create a new block
+        block = Block(len(self.chain), data, datetime.datetime.now(), previous_hash, 0, 0)
         self.chain.append(block)
 
         return block
 
+    # print the chain
     def print_chain(self):
         for block in self.chain:
-            print(block.id, block.data, block.date, block.hash, block.previous_hash)
+            print(block.id, block.data, block.timestamp, block.hash, block.previous_hash)
 
 blockchain = Blockchain()
 blockchain.add_block("First Block")
