13c4db800ebb004cb179966d39f12938b010f9ec
Collin Stratton <Collin.Stratton00@gmail.com>
2022-04-04 13:40:38 -0700

    got two cryptomon testing to work

--- Cryptomon/test/cryptomon.js
+++ Cryptomon/test/cryptomon.js
@@ -8,6 +8,8 @@ const creator = artifacts.require("./creator.sol");
  */
 
 contract('cryptomon', async accounts => {
+  /*
+
   it("should assert true", async () => {
     let instance = await creator.deployed();
     let cryptomonID = await instance.createCryptomon("Citromon", 10, 10, 10);
@@ -35,23 +37,62 @@ contract('cryptomon', async accounts => {
 
   it("should assert true", async () => {
     let instance = await creator.deployed();
-    let cryptomonID = await instance.createCryptomon("Citromon", 10, 10, 10);
-    let moveID = await instance.createMove("The Lick", 10);
-    moveID = await instance.createMove("Suckle", 69);
-    let temp = await instance.addMove(0, 0);
-    temp = await instance.addMove(0, 1);
 
-    let c = await instance.cryptomonList(0);
+    // let cryptomonID = await instance.createCryptomon("Citromon", 10, 10, 10);
+    // let moveID = await instance.createMove("The Lick", 10);
+    // moveID = await instance.createMove("Suckle", 69);
+    // let temp = await instance.addMove(0, 0);
+    // temp = await instance.addMove(0, 1);
 
-    cryptomonID = await instance.createCryptomon("Artzimon", 10, 10, 10);
-    moveID = await instance.createMove("Popping Germans", 10);
-    moveID = await instance.createMove("Debug", 69);
-    temp = await instance.addMove(1, 2);
-    temp = await instance.addMove(1, 3);
+    // let c = await instance.cryptomonList(0);
 
     let i = await instance.cryptomonList(1);
+    let cryptomonID = await instance.createCryptomon("Artzimon", 10, 10, 10);
+    let moveID = await instance.createMove("Popping Germans", 10);
+    moveID = await instance.createMove("Debug", 69);
+    let temp = await instance.addMove(1, 2);
+    temp = await instance.addMove(1, 3);
+
 
     assert.equal(i[1], "Artzimon");
+    // assert.equal(c[1], "Citromon");
+  })
+  
+  */
+
+  // test creating two cryptomons
+  it("should assert true", async () => {
+    // deploy creator contract
+    let instance = await creator.deployed();
+
+    // create first cryptomon with moves
+    let cryptomonID = await instance.createCryptomon("Citromon", 10, 10, 10);
+    let moveID = await instance.createMove("The Lick", 10);
+    let temp = await instance.addMove(0, 0);
+
+    // create second cryptomon with moves
+    let cryptomonID2 = await instance.createCryptomon("Citromon2", 10, 10, 10);
+    let moveID2 = await instance.createMove("The Lick2", 10);
+    let temp2 = await instance.addMove(1, 1);
+
+    // get cryptomon from list
+    let c = await instance.cryptomonList(0);
+    let c2 = await instance.cryptomonList(1);
+
+    // get moves from list
+    let m = await instance.movesList(0);
+    let m2 = await instance.movesList(1);
+    
+    // console.log(m2);
+
+    // assert the cryptomon names
     assert.equal(c[1], "Citromon");
+    assert.equal(c2[1], "Citromon2");
+
+    // assert that the first cryptomon has the correct moves
+    assert.equal(c[6][1], "The Lick");
+
+    // assert that the second cryptomon has the correct moves
+    assert.equal(c2[6][1], "The Lick2");
   })
 });
