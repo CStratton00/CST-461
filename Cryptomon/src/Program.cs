a5053e08d4624b51e684827be334e56c46bbd968
Collin Stratton <Collin.Stratton00@gmail.com>
2022-04-09 14:27:23 -0700

    got c# stuff working, need to test with contracts

---
+++ Cryptomon/src/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Nethereum.Web3;
+using Cryptomon.creator;
+
+namespace Cryptomon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
