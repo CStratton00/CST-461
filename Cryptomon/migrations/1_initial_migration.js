c4d11ca33725062f4ff8f3554cc53e42c4a66c41
Collin Stratton <Collin.Stratton00@gmail.com>
2022-04-15 16:32:02 -0700

    tried getting c# scripts communicating with the blockchain

--- Cryptomon/migrations/1_initial_migration.js
+++ Cryptomon/migrations/1_initial_migration.js
@@ -1,11 +1,23 @@
 const creator = artifacts.require("./creator.sol");
-const cryptomon = artifacts.require("./cryptomon.sol")
+// const cryptomon = artifacts.require("./cryptomon.sol")
 //const move = artifacts.require("./move.sol");
 
 module.exports = function (deployer) {
-    //let instance = creator.deployed();
     deployer.deploy(creator);
-    //console.log("sa");
-    deployer.deploy(cryptomon, "Citromon", 2, 2, 2, "0xb533568585A5bd3CbEBDF8982e6d9586aAb0c68F");
-    //console.log("fdsahf;");
+    // deployer.deploy(cryptomon, "Citromon", 2, 2, 2, "0x1ADE4fa56c1175B4155a49368f3F550CEc3d2dCc");
+
+    let instance = await creator.deployed();
+
+    // deploy a cryptomon
+    // let cryptomonID = await instance.createCryptomon("Citromon", 10, 10, 10);
+    // let moveID = await instance.createMove("The Lick", 10);
+    // moveID = await instance.createMove("Suckle", 69);
+    // let temp = await instance.addMove(0, 0);
+    // temp = await instance.addMove(0, 1);
+
+    // let cryptomonID2 = await instance.createCryptomon("Citromon2", 10, 10, 10);
+    // let moveID2 = await instance.createMove("The Lick2", 10);
+    // moveID2 = await instance.createMove("Suckle2", 69);
+    // let temp2 = await instance.addMove(1, 2);
+    // temp2 = await instance.addMove(1, 3);
 };
